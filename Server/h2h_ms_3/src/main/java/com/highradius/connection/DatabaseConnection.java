235399ef9c20aa7ccd08c89414fc85cafe147a33
dikshya-pahari <dikshyapahari977@gmail.com>
2024-05-16 17:38:17 +0530

    add initial files

---
+++ Server/h2h_ms_3/src/main/java/com/highradius/connection/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.highradius.connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+	// connection url jdbc:mysql://ipAddress:portnumber/databasename
+		private static String URL = "jdbc:mysql://localhost:3306/oap_h2h";
+		// Database username
+		private static String USER = "root";
+		// Database password
+		private static String PASS = "sachin1751";
+
+		public Connection connect() throws SQLException {
+			Connection con=null;
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				 con = DriverManager.getConnection(URL, USER, PASS);
+				if (con == null) {
+					System.out.println("Connection cannot be established");
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			return con;
+
+}
+}
